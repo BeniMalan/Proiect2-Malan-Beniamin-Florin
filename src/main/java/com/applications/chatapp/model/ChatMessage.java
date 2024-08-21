e31d05a9824828fda0f46438e6ff721aeeaa69b4
BeniMalan <malanbeniaminflorin@gmail.com>
2024-08-21 13:18:40 +0300

    first commit

---
+++ src/main/java/com/applications/chatapp/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.applications.chatapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+
+public class ChatMessage {
+    private String nickname;
+    private String  content;
+    private Date time;
+
+}
